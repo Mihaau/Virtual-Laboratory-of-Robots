47a6cb1225f96a310bc59c0659697d45b907919c
michal.pryba.mail@gmail.com <michal.pryba.mail@gmail.com>
2024-03-25 15:27:24 +0100

    clean up project directories add xmake support

---
+++ xmake.lua
@@ -0,0 +1,7 @@
+add_rules("mode.debug", "mode.release")
+add_requires("glad", "glfw")
+target("robolab")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    add_includedirs("include")
+    add_packages("glad", "glfw")
