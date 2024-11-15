3b3c54ee28e981de38d686344798943e6f34b21d
Mihau <michal.pryba.mail@gmail.com>
2024-11-15 23:01:36 +0100

    Fix number align

--- src/codeEditor.cpp
+++ src/codeEditor.cpp
@@ -16,49 +16,51 @@ CodeEditor::~CodeEditor() {
 void CodeEditor::Draw(const char* label) {
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(8, 8));
     
-    // Editor settings
     if (ImGui::CollapsingHeader("Ustawienia edytora")) {
         ImGui::Checkbox("Numery linii", &showLineNumbers);
         
-        if (ImGui::Button("Zapisz")) {
-            if (!filename.empty()) {
-                SaveToFile(filename);
-            }
+        if (ImGui::Button("Zapisz") && !filename.empty()) {
+            SaveToFile(filename);
         }
         ImGui::SameLine();
-        if (ImGui::Button("Wczytaj")) {
-            if (!filename.empty()) {
-                LoadFromFile(filename);
-            }
+        if (ImGui::Button("Wczytaj") && !filename.empty()) {
+            LoadFromFile(filename);
         }
     }
 
-    // Main editor window
     float reserveHeight = ImGui::GetStyle().ItemSpacing.y + ImGui::GetFrameHeightWithSpacing();
     ImVec2 size = ImGui::GetContentRegionAvail();
     size.y -= reserveHeight;
 
-    // Line numbers
     if (showLineNumbers) {
         ImGui::BeginChild("##line_numbers", ImVec2(30, size.y), true);
-        int lineCount = 1;
+        
+        // Oblicz całkowitą liczbę linii
+        int totalLines = 1;
         for (const char* c = buffer; *c != '\0'; c++) {
-            if (*c == '\n') {
-                ImGui::Text("%d", lineCount++);
-            }
+            if (*c == '\n') totalLines++;
         }
+
+        // Pobierz style i metryki
+        float lineHeight = ImGui::GetTextLineHeight();
+        float padding = ImGui::GetStyle().FramePadding.y;
+        
+        // Ustaw początkową pozycję Y uwzględniającą padding
+        float currentY = padding;
+        
+        // Renderuj numery linii
+        for (int i = 1; i <= totalLines; i++) {
+            ImGui::SetCursorPosY(currentY);
+            ImGui::Text("%d", i);
+            currentY += lineHeight;
+        }
+        
         ImGui::EndChild();
         ImGui::SameLine();
         size.x -= 30;
     }
 
-    // Code editor
-    ImGui::InputTextMultiline(label, 
-        buffer, 
-        bufferSize, 
-        size,
-        flags);
-
+    ImGui::InputTextMultiline(label, buffer, bufferSize, size, flags);
     ImGui::PopStyleVar();
 }
 
