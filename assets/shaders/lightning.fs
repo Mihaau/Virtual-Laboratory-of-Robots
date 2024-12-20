b2bfa46aa609db887d1eeb723cd456d621036265
Mihaau <michal.pryba.mail@gmail.com>
2024-12-20 03:18:00 +0100

    add robot model and fix materials

--- assets/shaders/lightning.fs
+++ assets/shaders/lightning.fs
@@ -1,17 +1,13 @@
 #version 330
-
 // Input vertex attributes (from vertex shader)
 in vec3 fragPosition;
 in vec2 fragTexCoord;
 in vec4 fragColor;
 in vec3 fragNormal;
-
 // Input uniform values
 uniform sampler2D texture0;
 uniform vec4 colDiffuse;
 uniform vec3 viewPos;
-uniform vec4 materialColor;
-
 // Input lighting values
 uniform vec4 ambient;
 struct Light {
@@ -21,12 +17,9 @@ struct Light {
     vec3 target;
     vec4 color;
 };
-
 const int maxLights = 4;
 uniform Light lights[maxLights];
-
 out vec4 finalColor;
-
 void main()
 {
     // Texel color fetching from texture sampler
@@ -35,14 +28,12 @@ void main()
     vec3 normal = normalize(fragNormal);
     vec3 viewD = normalize(viewPos - fragPosition);
     vec3 specular = vec3(0.0);
-
     // Calculate lighting on current fragment
     for (int i = 0; i < maxLights; i++)
     {
         if (lights[i].enabled == 1)
         {
             vec3 light = vec3(0.0);
-
             if (lights[i].type == 0)    // Directional light
             {
                 light = -normalize(lights[i].target - lights[i].position);
@@ -51,20 +42,15 @@ void main()
             {
                 light = normalize(lights[i].position - fragPosition);
             }
-
             float NdotL = max(dot(normal, light), 0.0);
             lightDot += lights[i].color.rgb*NdotL;
-
             float specCo = 0.0;
             if (NdotL > 0.0) specCo = pow(max(0.0, dot(viewD, reflect(-(light), normal))), 16.0);
             specular += specCo;
         }
     }
-
-finalColor = (texelColor*((colDiffuse + vec4(specular, 1.0))*vec4(lightDot, 1.0)));
-finalColor *= materialColor;  // Pomnóż przez kolor materiału
-finalColor += texelColor*(ambient/10.0);
-
+    finalColor = (texelColor*((colDiffuse + vec4(specular, 1.0))*vec4(lightDot, 1.0)));
+    finalColor += texelColor*(ambient/10.0);
     // Gamma correction
     finalColor = pow(finalColor, vec4(1.0/2.2));
 }
